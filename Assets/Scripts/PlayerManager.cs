8abfcd54db0d0183cb20a5356fc06366f30d5002
Max Kaiser <max@kaiser.family>
2025-04-21 17:03:25 +0200

    player and skill manager
    added playermanager and skillmanager singleton

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+   public static PlayerManager instance;
+   public Player player;
+
+   private void Awake()
+   {
+      if (instance != null)
+         Destroy(instance.gameObject);
+      else
+         instance = this;
+   }
+}
